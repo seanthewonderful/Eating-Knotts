a069b20610a688f8ce1f6f5f0480d6c23ae0bf48
Sean Fagan <faganseanp@gmail.com>
2023-12-20 11:12:17 -0700

    popup styling, map attribution expanded

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,6 +1,7 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import MapMarker from "./MapMarker.jsx";
 import SetViewOnClick from "./SetViewOnClick.jsx";
+import reactLeaflet from "../../../public/icons/reactLeaflet.svg";
 
 export default function Map({ allRestaurants }) {
   const markers = allRestaurants.map((rest) => {
@@ -17,7 +18,7 @@ export default function Map({ allRestaurants }) {
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution={`&copy; <img id="react-leaflet-logo" src=${reactLeaflet} alt="" /> <a href="https://react-leaflet.js.org/">React-Leaflet</a>, &copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>`}
         url={`https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=${
           import.meta.env.VITE_THUNDERFOREST_API_KEY
         }`}
@@ -25,13 +26,12 @@ export default function Map({ allRestaurants }) {
       />
 
       <TileLayer
-        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution={`&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>`}
         url="https://tiles.stadiamaps.com/tiles/stamen_terrain_labels/{z}/{x}/{y}{r}.{ext}"
         maxZoom={22}
         ext="png"
       />
       <SetViewOnClick animateRef={true} />
-
       {markers}
     </MapContainer>
   );
--- src/components/map/MapPopup.jsx
+++ src/components/map/MapPopup.jsx
@@ -1,35 +1,44 @@
-import { Container, Row, Col } from "react-bootstrap"
-import { Popup } from "react-leaflet"
-import { NavLink } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Popup } from "react-leaflet";
+import { NavLink, useNavigate } from "react-router-dom";
+import StarAvg from "../StarAvg";
+import { calculateAvgStars } from "../../assets/funx.js";
 
 export default function MapPopup({ restaurant }) {
+  const [avgStars, setAvgStars] = useState(0);
 
+  const navigate = useNavigate();
+
+  const calculateStarAvg = () => {
+    setAvgStars(calculateAvgStars(restaurant));
+  };
+
+  useEffect(() => {
+    calculateStarAvg();
+  }, []);
 
   return (
     <Popup>
-
       <Container>
-
         <Row id="popup-card">
-
           <NavLink to={`/restaurant/${restaurant.restaurantId}`}>
-          <Row>
-            <h2>{restaurant.name}</h2>
-          </Row>
+            <Row>
+              <h4>{restaurant.name}</h4>
+            </Row>
           </NavLink>
 
           <Row>
-            <h4>{restaurant.expense}</h4>
+            <p id="restaurant-expense">{restaurant.expense}</p>
           </Row>
 
           <Row>
-            <p>{restaurant.description}</p>
+            <p>
+              <StarAvg starAvg={avgStars} />({restaurant.ratings.length})
+            </p>
           </Row>
-          
         </Row>
-
       </Container>
-
     </Popup>
-  )
+  );
 }
